93b4a547b9c365b9f2faf9864b99536fed2f4f3d
Aniket150293 <aniketbansode15@gmail.com>
2024-06-28 20:10:02 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const users = [
-    { username: 'admin', password: 'CrackJack' },
-    { username: 'Hrishikesh', password: 'hrishi@rsm' }
+  { username: "admin", password: "CrackJack" },
+  { username: "Hrishikesh", password: "hrishi@rsm" },
+  { username: "Trial", password: "freeversion" },
 ];
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(user => user.username === username && user.password === password);
-    
-    if (user) {
-        res.json({ success: true, redirectUrl: '/dashboard.html' });
-    } else {
-        res.json({ success: false });
-    }
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (user) {
+    res.json({ success: true, redirectUrl: "/dashboard.html" });
+  } else {
+    res.json({ success: false });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
